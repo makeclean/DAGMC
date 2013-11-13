077cacc89cc4f846eac0b3e72fe4e77832cf19e1
Andrew Davis <davisa@engr.wisc.edu>
2013-11-12 19:43:00 -0600

    added more surface mesh functions

--- MCNP5/dagmc/SurfaceMeshTally.cpp
+++ MCNP5/dagmc/SurfaceMeshTally.cpp
@@ -24,17 +24,28 @@ SurfaceMeshTally::SurfaceMeshTally(const TallyInput& input)
     : MeshTally(input),
       mb (new moab::Core())
 {
-   std::cout << "Creating surface mesh tally" << input.tally_id 
+  ErrorCode rval;
+
+  std::cout << "Creating surface mesh tally" << input.tally_id 
             << ", input: " << input_filename 
             << ", output: " << output_filename << std::endl;
 
-   parse_tally_options(); // parse the tally options
+   //   parse_tally_options(); // parse the tally options
  
-   set_tally_meshset(); // set the meshset
+   // set_tally_meshset(); // set the meshset
+
+   // unite the meshset 
+   rval = mb->unite_meshset( tally_mesh_set, loaded_file_set );
+   if(rval != MB_SUCCESS)
+     {
+       std::cout << "Failed to unite meshset" << std::endl;
+       exit(1);
+     }
+   assert (rval == MB_SUCCESS);
 
    // reduce the loaded MOAB mesh set to include only 2D elements
    Range facets;
-   ErrorCode rval = reduce_meshset_to_2D(mb, tally_mesh_set, facets);  
+   rval = reduce_meshset_to_2D(mb, tally_mesh_set, facets);  
    if(rval != MB_SUCCESS)
      {
        std::cout << "Failed to reduce meshset to 2D" << std::endl;
@@ -43,7 +54,7 @@ SurfaceMeshTally::SurfaceMeshTally(const TallyInput& input)
    assert (rval == MB_SUCCESS);
 
    // initialize MeshTally::tally_points to include all mesh cells
-   set_tally_points(all_tets);
+   set_tally_points(facets);
 
    // precompute the surface normals
    rval = compute_surface_normals(facets);
@@ -56,7 +67,7 @@ SurfaceMeshTally::SurfaceMeshTally(const TallyInput& input)
    assert (rval == MB_SUCCESS);
 
    // build the kd-trees
-   build_trees(all_tets);
+   build_trees(facets);
 
 }
 //---------------------------------------------------------------------------//
@@ -77,6 +88,20 @@ void SurfaceMeshTally::parse_tally_options()
   return;
 }
 
+//---------------------------------------------------------------------------//
+void SurfaceMeshTally::build_trees (Range &all_facets)
+{
+  // prepare to build KD tree and OBB tree
+  std::cout << "  Tally mesh has " << all_facets.size() << " triangles." << std::flush;
+
+  // build KD tree of all tetrahedra and triangles
+  std::cout << "  Building KD tree of size " << all_facets.size() << "... " << std::flush;
+  kdtree = new AdaptiveKDTree( mb );
+  kdtree->build_tree( all_facets, kdtree_root );
+  std::cout << "done." << std::endl << std::endl;
+  return;
+}
+
 //
 // Computes the score for the given event, 
 // 
@@ -87,7 +112,7 @@ void SurfaceMeshTally::compute_score(const TallyEvent &event)
   // would like to record surface crossing event
   if (event.type != TallyEvent::TRACK) return;
 
-  hit_facets = get_intersections( kdtree, kdtree_root,event.direction,event.position,
+  hit_facets = get_intersections(event.direction,event.position,
 				 event.track_length);
   num_hits = hit_facets.size();
   if ( num_hits == 0 )
@@ -107,28 +132,28 @@ void SurfaceMeshTally::compute_score(const TallyEvent &event)
 /*
 * Wrapper function, return the surface normal of the triangle 
 */
-CartVect SurfaceMeshTally::surface_normal(MBEntityHandle triangle)
+CartVect SurfaceMeshTally::surface_normal(const EntityHandle triangle)
 {
-  const MBEntityHandle *connectivity; 
+  const EntityHandle *connectivity; 
   int number_nodes = 0;
-  MBErrorCode result = MBI()->get_connectivity(triangle,connectivity,number_nodes);
+  ErrorCode result = mb->get_connectivity(triangle,connectivity,number_nodes);
  
   //get the coordinates of each node
-  MBCartVect nodes[3],normal;
+  CartVect nodes[3],normal;
   for ( int i = 0 ; i <= 2 ; i++ )
     {
-      result = MBI()->get_coords(&(connectivity[i]),1,nodes[i].array());
+      result = mb->get_coords(&(connectivity[i]),1,nodes[i].array());
     }
   
-  MBCartVect V1 = nodes[1]-nodes[0];
-  MBCartVect V2 = nodes[2]-nodes[1];
-  MBCartVect CrossProd = V1*V2;
+  CartVect V1 = nodes[1]-nodes[0];
+  CartVect V2 = nodes[2]-nodes[1];
+  CartVect CrossProd = V1*V2;
   //    return CrossProd.normalize();
-  MBCartVect normalize(CrossProd); 
+  CartVect normalize(CrossProd); 
   return CrossProd;
 }
   
-double SurfaceMeshTally::get_angle( const MBCartVect u, const MBCartVect v )
+double SurfaceMeshTally::get_angle( const CartVect u, const CartVect v )
 {
   double angle=(u % v) / std::sqrt((u % u) * (v % v));
   if (angle>1.) angle=1.;
@@ -161,9 +186,8 @@ ErrorCode SurfaceMeshTally::compute_surface_normals(const Range &all_facets)
 
 // return the intersection data, could return more than one of there are multiple surfaces
 // in each meshset
-std::vector<MBEntityHandle> SurfaceMeshTally::get_intersections(MBAdaptiveKDTree* kdtree,
-				MBEntityHandle kdtree_root, double dir[3], double box_center[3],
-				double ray_length)
+std::vector<MBEntityHandle> SurfaceMeshTally::get_intersections(const double dir[3], const double box_center[3],
+				const double ray_length)
 {
   // we do not keep the intersection distances
   std::vector<MBEntityHandle> ret_triangles;
