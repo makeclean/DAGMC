5e9832c50a01a82792d7b4d4b91d694a28c23f7c
Andrew Davis <davisa@engr.wisc.edu>
2015-05-12 22:22:10 -0500

    added geant4 source and tidied library

--- geant4/build/src/ExN01PrimaryGeneratorAction.cc
+++ geant4/build/src/ExN01PrimaryGeneratorAction.cc
@@ -16,52 +16,56 @@
 #include "Randomize.hh"
 #include "G4PhysicalConstants.hh"
 
+#include "source_tf.h"
 
 ExN01PrimaryGeneratorAction::ExN01PrimaryGeneratorAction()
-  :G4VUserPrimaryGeneratorAction(), fParticleGun(0)
+  :G4VUserPrimaryGeneratorAction(), particleGun(0)
 {
-  fParticleGun = new G4GeneralParticleSource();
-  /*
+  //  fParticleGun = new G4GeneralParticleSource();
+  //fParticleGun->SetParticleDefinition(particleTable->FindParticle(particleName="neutron"));
+  //fParticleGun->SetParticleEnergy(14.0*MeV);
+  
   G4int n_particle = 1;
-  particleGun = new G4GeneralParticleSource();
-  //  particleGun = new G4ParticleGun(n_particle);
+  particleGun = new G4ParticleGun(n_particle);
 
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName;
   particleGun->SetParticleDefinition(particleTable->FindParticle(particleName="neutron"));
-  //  particleGun->SetParticleEnergy(14.0*MeV);
-  */
+  setup_();
 }
 
 ExN01PrimaryGeneratorAction::~ExN01PrimaryGeneratorAction()
 {
-  delete fParticleGun;
+  delete particleGun;
 }
 
 void ExN01PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-  fParticleGun->GeneratePrimaryVertex(anEvent) ;
+  //  fParticleGun->GeneratePrimaryVertex(anEvent) ;
 
+  // 
   
-  //G4double y = 1.0*(G4UniformRand()-0.5);
-  //  G4double z = 1.0*(G4UniformRand()-0.5);
-  //  particleGun->SetParticlePosition(G4ThreeVector(1.24*cm,1.24*cm,100.0*cm));
-  // particleGun->SetParticlePosition(G4ThreeVector(0.*cm,0.*cm,0.*cm));
-  //  particleGun->SetParticleMomentumDirection(G4ThreeVector(1.0,0.0,0.0));
+  G4double x,y,z;
+  G4double energy,weight;
+  sample_linear_(20.,-20.,G4UniformRand(),x);
+  sample_linear_(20.,-20.,G4UniformRand(),y);
+  sample_linear_(20.,-20.,G4UniformRand(),z);
+
+  sample_(G4UniformRand(),G4UniformRand(),energy,weight);
+  particleGun->SetParticleEnergy(energy*MeV);
+  particleGun->SetParticlePosition(G4ThreeVector(x*cm,y*cm,z*cm));
 
   //  particleGun->SetParticlePosition(G4ThreeVector(-10.0*cm,y*cm,z*cm));
   //  G4int i = anEvent->GetEventID() % 3;
 
-  /*
   G4double cosTheta = 2*G4UniformRand() - 1.;
   G4double phi = twopi*G4UniformRand();
   G4double sinTheta = std::sqrt(1. - cosTheta*cosTheta);
   G4double ux = sinTheta*std::cos(phi),
     uy = sinTheta*std::sin(phi),
     uz = cosTheta;
-  //particleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));
-  */  
-
+  particleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));
+  particleGun->GeneratePrimaryVertex(anEvent);
 }
 
 
