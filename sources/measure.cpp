e3a5b84a8300b2fa47cc8e5f0fc5d620f50fb6f1
Andrew Davis <davisa@engr.wisc.edu>
2016-08-22 18:33:53 -0500

    Ran astyle on the source tree

--- sources/measure.cpp
+++ sources/measure.cpp
@@ -27,70 +27,83 @@
 
 #include "measure.h"
 
-class CartVect {
-  private:
-    double coords[3];
-    
-  public:
-  
-    inline CartVect() {}
-    
-    inline CartVect( double tx, double ty, double tz ) { set(tx,ty,tz); }
-    
-    inline CartVect( const CartVect& other ) { set( other.coords); }
-    
-    inline void set( double tx, double ty, double tz )
-      { coords[0] = tx; coords[1] = ty; coords[2] = tz; }
-    
-    inline void set( const double* c )
-      { coords[0] = c[0]; coords[1] = c[1]; coords[2] = c[2]; }
-    
-    inline double x() const { return coords[0]; }
-    inline double y() const { return coords[1]; }
-    inline double z() const { return coords[2]; }
-  
-    inline CartVect& operator+=( const CartVect& other )
-    {
-      coords[0] += other.coords[0];
-      coords[1] += other.coords[1];
-      coords[2] += other.coords[2];
-      return *this;
-    }
-    
-    inline CartVect& operator-=( const CartVect& other )
-    {
-      coords[0] -= other.coords[0];
-      coords[1] -= other.coords[1];
-      coords[2] -= other.coords[2];
-      return *this;
-    }
-    
-    inline CartVect& operator*=( const CartVect& other );
+class CartVect
+{
+ private:
+  double coords[3];
 
-    inline double lensqr() const;
-    
-    inline double len() const;
-    
-    inline CartVect operator~( ) const;
+ public:
 
-      
-    inline CartVect& operator*=( double a )
-    {
-      coords[0] *= a;
-      coords[1] *= a;
-      coords[2] *= a;
-      return *this;
-    }
-    
-    inline CartVect& operator/=( double a )
-    {
-      coords[0] /= a;
-      coords[1] /= a;
-      coords[2] /= a;
-      return *this;
-    }
-   
-    
+  inline CartVect() {}
+
+  inline CartVect( double tx, double ty, double tz ) {
+    set(tx,ty,tz);
+  }
+
+  inline CartVect( const CartVect& other ) {
+    set( other.coords);
+  }
+
+  inline void set( double tx, double ty, double tz ) {
+    coords[0] = tx;
+    coords[1] = ty;
+    coords[2] = tz;
+  }
+
+  inline void set( const double* c ) {
+    coords[0] = c[0];
+    coords[1] = c[1];
+    coords[2] = c[2];
+  }
+
+  inline double x() const {
+    return coords[0];
+  }
+  inline double y() const {
+    return coords[1];
+  }
+  inline double z() const {
+    return coords[2];
+  }
+
+  inline CartVect& operator+=( const CartVect& other ) {
+    coords[0] += other.coords[0];
+    coords[1] += other.coords[1];
+    coords[2] += other.coords[2];
+    return *this;
+  }
+
+  inline CartVect& operator-=( const CartVect& other ) {
+    coords[0] -= other.coords[0];
+    coords[1] -= other.coords[1];
+    coords[2] -= other.coords[2];
+    return *this;
+  }
+
+  inline CartVect& operator*=( const CartVect& other );
+
+  inline double lensqr() const;
+
+  inline double len() const;
+
+  inline CartVect operator~( ) const;
+
+
+  inline CartVect& operator*=( double a ) {
+    coords[0] *= a;
+    coords[1] *= a;
+    coords[2] *= a;
+    return *this;
+  }
+
+  inline CartVect& operator/=( double a ) {
+    coords[0] /= a;
+    coords[1] /= a;
+    coords[2] /= a;
+    return *this;
+  }
+
+
 };
 
 inline CartVect operator+( const CartVect& v1, const CartVect& v2 )
@@ -124,19 +137,25 @@ inline CartVect CartVect::operator~() const
   double invlen = 1.0 / len();
   return CartVect( invlen * x(), invlen * y(), invlen * z() );
 }
-     
+
 inline CartVect& CartVect::operator*=( const CartVect& other )
-      { return *this = *this * other; }
+{
+  return *this = *this * other;
+}
 
 inline double CartVect::lensqr() const
-      { return *this % *this; }
-    
+{
+  return *this % *this;
+}
+
 inline double CartVect::len() const
-      { return sqrt(lensqr()); }
- 
+{
+  return sqrt(lensqr());
+}
+
 inline static double tet_volume( const CartVect& v0,
                                  const CartVect& v1,
-                                 const CartVect& v2, 
+                                 const CartVect& v2,
                                  const CartVect& v3 )
 {
   return 1./6. * ( ((v1 - v0) * (v2 - v0)) % (v3 - v0) );
@@ -155,46 +174,43 @@ double measure( moab::EntityType type,
                 const double* vertex_coordinates )
 {
   const CartVect* coords = reinterpret_cast<const CartVect*>(vertex_coordinates);
-  switch( type )
-  {
-    case moab::MBEDGE:
-      return (coords[0] - coords[1]).len();
-    case moab::MBTRI:
-      return 0.5 * ((coords[1] - coords[0]) * (coords[2] - coords[0])).len();
-    case moab::MBQUAD:
-      num_vertices = 4;
-    case moab::MBPOLYGON:
-    {
-      CartVect mid(0,0,0);
-      for (int i = 0; i < num_vertices; ++i)
-        mid += coords[i];
-      mid /= num_vertices;
-      
-      double sum = 0.0;
-      for (int i = 0; i < num_vertices; ++i)
-      {
-        int j = (i+1)%num_vertices;
-        sum += ((mid - coords[i]) * (mid - coords[j])).len();
-      }
-      return 0.5 * sum;
+  switch( type ) {
+  case moab::MBEDGE:
+    return (coords[0] - coords[1]).len();
+  case moab::MBTRI:
+    return 0.5 * ((coords[1] - coords[0]) * (coords[2] - coords[0])).len();
+  case moab::MBQUAD:
+    num_vertices = 4;
+  case moab::MBPOLYGON: {
+    CartVect mid(0,0,0);
+    for (int i = 0; i < num_vertices; ++i)
+      mid += coords[i];
+    mid /= num_vertices;
+
+    double sum = 0.0;
+    for (int i = 0; i < num_vertices; ++i) {
+      int j = (i+1)%num_vertices;
+      sum += ((mid - coords[i]) * (mid - coords[j])).len();
     }
-    case moab::MBTET:
-      return tet_volume( coords[0], coords[1], coords[2], coords[3] ) ;
-    case moab::MBPYRAMID:
-      return tet_volume( coords[0], coords[1], coords[2], coords[4] ) +
-             tet_volume( coords[0], coords[2], coords[3], coords[4] ) ;
-    case moab::MBPRISM:
-      return tet_volume( coords[0], coords[1], coords[2], coords[5] ) +
-             tet_volume( coords[3], coords[5], coords[4], coords[0] ) +
-             tet_volume( coords[1], coords[4], coords[5], coords[0] ) ;
-    case moab::MBHEX:
-      return tet_volume( coords[0], coords[1], coords[3], coords[4] ) +
-             tet_volume( coords[7], coords[3], coords[6], coords[4] ) +
-             tet_volume( coords[4], coords[5], coords[1], coords[6] ) +
-             tet_volume( coords[1], coords[6], coords[3], coords[4] ) +
-             tet_volume( coords[2], coords[6], coords[3], coords[1] ) ;
-    default:
-      return 0.0;
+    return 0.5 * sum;
+  }
+  case moab::MBTET:
+    return tet_volume( coords[0], coords[1], coords[2], coords[3] ) ;
+  case moab::MBPYRAMID:
+    return tet_volume( coords[0], coords[1], coords[2], coords[4] ) +
+           tet_volume( coords[0], coords[2], coords[3], coords[4] ) ;
+  case moab::MBPRISM:
+    return tet_volume( coords[0], coords[1], coords[2], coords[5] ) +
+           tet_volume( coords[3], coords[5], coords[4], coords[0] ) +
+           tet_volume( coords[1], coords[4], coords[5], coords[0] ) ;
+  case moab::MBHEX:
+    return tet_volume( coords[0], coords[1], coords[3], coords[4] ) +
+           tet_volume( coords[7], coords[3], coords[6], coords[4] ) +
+           tet_volume( coords[4], coords[5], coords[1], coords[6] ) +
+           tet_volume( coords[1], coords[6], coords[3], coords[4] ) +
+           tet_volume( coords[2], coords[6], coords[3], coords[1] ) ;
+  default:
+    return 0.0;
   }
 }
-      
+
