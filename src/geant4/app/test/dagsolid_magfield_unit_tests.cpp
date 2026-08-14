7f952d45e178cd932602c3ab7ae5b770f6bda947
Andrew Davis <andrew.davis@ukaea.uk>
2026-08-14 16:27:36 +0100

    still going

--- src/geant4/app/test/dagsolid_magfield_unit_tests.cpp
+++ src/geant4/app/test/dagsolid_magfield_unit_tests.cpp
@@ -173,7 +173,7 @@ TEST_F(DagSolidWireMagneticFieldTest, sample_test) {
   // should be equal to mu0*I/2pi*r = 20
   EXPECT_NEAR(expected, sqrt(field[0]*field[0] + field[1]*field[1]), 1e-6);
   EXPECT_NEAR(-expected, field[0], 1e-6);
-  EXPECT_EQ(0, field[1]);
+  EXPECT_EQ(0.0, field[1]);
   EXPECT_EQ(0.0, field[2]);
 
   // test point further away
@@ -186,7 +186,7 @@ TEST_F(DagSolidWireMagneticFieldTest, sample_test) {
   // should be equal to mu0*I/2pi*r = 2
   EXPECT_NEAR(expected, sqrt(field[0]*field[0] + field[1]*field[1]), 1e-6);
   EXPECT_NEAR(-expected, field[0],1e-6);
-  EXPECT_EQ(0, field[1], 1e-6);
+  EXPECT_EQ(0.0, field[1]);
   EXPECT_EQ(0.0, field[2]);
 
   
